5c557e39a6414b4ec10f11d901b69e014009fb1e
angularexample <info@angularexample.com>
2025-08-29 09:37:57 -0400

    app.tsx reduce code

--- apps/remote3/src/app/app.tsx
+++ apps/remote3/src/app/app.tsx
@@ -6,7 +6,7 @@ import r2wc from '@r2wc/react-to-web-component';
 export function App() {
   return (
     <div>
-      <NxWelcome title="remote3" />
+      <NxWelcome title="remote3"/>
     </div>
   );
 }
@@ -14,10 +14,8 @@ export function App() {
 export function defineReactWebComponent() {
   // Define the new custom element with the element name for the React Web Component.
   if (!customElements.get('wc-remote3')) {
-    if (!customElements.get('wc-remote3')) {
-      // This is where we convert the React component to a Web Component
-      customElements.define('wc-remote3', r2wc(App));
-    }
+    // This is where we convert the React component to a Web Component
+    customElements.define('wc-remote3', r2wc(App));
   }
 }
 
